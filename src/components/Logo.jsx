eaf9ac23908ee34483031648e139ea172264bfdd
Ryan Mehta <ryan.mehta@gmail.com>
2023-03-17 08:49:27 -0700

    fix logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,12 +3,6 @@ import logo from "../../static/img/logo.png";
 
 export function Logo(props) {
   return (
-    <img
-      className="mx-auto block"
-      alt="zkSig logo"
-      src={logo}
-      width="150"
-      height="0"
-    />
+    <img className="mx-auto block" alt="zkSig logo" src={logo} width="150" />
   );
 }
